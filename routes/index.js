7aad91e2d6bd23cb033578d01ec8d33707ffc04a
ingelieur <ingelieur@gmail.com>
2017-06-07 04:21:14 +0700

    restful crud api, authentication, and authorization

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const user = require('../controllers/users');
-
-router.post('/signin', user.signIn);
-router.post('/signup', user.signUp);
-
-module.exports = router;
--- routes/users.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const user = require('../controllers/users');
-
-router.get('/:id', user.authentication, user.showOne);
-router.put('/:id', user.authentication, user.changePass);
-
-module.exports = router;
