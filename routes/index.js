483ec0bc4660bf564802ad5e802a0860f683ec08
ingelieur <ingelieur@gmail.com>
2017-06-11 03:44:45 +0700

    ALIVE

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const user = require('../controllers/users');
 
+router.get('/', (req,res) => {
+  res.send(`I'M ALIVE!`)
+})
 router.post('/signin', user.signIn);
 router.post('/signup', user.create);
 
